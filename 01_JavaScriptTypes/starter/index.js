5a522126d2d84af601d5153a1408816f87ba5ad1
treesacherian <treesa.cherian@gmail.com>
2023-11-21 13:36:36 +0000

    code changed

--- 01_JavaScriptTypes/starter/index.js
+++ 01_JavaScriptTypes/starter/index.js
@@ -1,2 +1,9 @@
 
 
+let numTest = 45.324568; 
+console.log(numTest);
+let twoDecimalPoints = numTest.toFixed(2);
+ console.log(twoDecimalPoints);
+ let stringTest = `I am the very model of a modern major general`; 
+ let indexOfM = stringTest.indexOf(`m`); 
+ console.log(indexOfM);
--- 03a_JavaScriptConditionals/starter/index.js
+++ 03a_JavaScriptConditionals/starter/index.js
@@ -1,2 +1,33 @@
+let age=80;
+if (age <= 17)
+ { console.log("Underage"); 
+} 
+else if (age<=65 && age>=18){
+    console.log("Insurable"); 
+}
+else { 
+    console.log("out of range");
+ }
+
+let x=375;
+if(x % 3 ==0 && x % 5 !=0){
+    console.log('Fizz');
+}
+else if (x % 5 ==0 && x % 3 !=0){
+    console.log('Buzz');
+}
+else if(x % 3 ==0 && x % 5 ==0){
+    console.log('Fizz Buzz');
+}
 
 
+// let xx=375;
+// if(xx % 3 ==0 ){
+//     console.log('Fizz');
+// }
+// if (xx % 5 ==0 ){
+//     console.log('Buzz');
+// }
+// if(xx % 3 ==0 && x % 5 ==0){
+//     console.log('Fizz Buzz');
+// }
